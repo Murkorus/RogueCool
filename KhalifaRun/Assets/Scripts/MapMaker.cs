a940fbfa2de8a886977b0b33ff94ad48bdb1e3c6
marcede9 <71291845+marcede9@users.noreply.github.com>
2023-12-13 15:02:59 +0100

    random room placement

--- KhalifaRun/Assets/Scripts/MapMaker.cs
+++ KhalifaRun/Assets/Scripts/MapMaker.cs
@@ -1,3 +1,4 @@
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,12 +13,58 @@ public class MapMaker : MonoBehaviour
     [SerializeField] List<GameObject> roomPrefabs;
     [SerializeField] GameObject startRoom;
     [SerializeField] GameObject bossRoom;
-
-
+    
+    
+    List<GameObject> roomList =new List<GameObject>();
     int numberOfDoors;
     void Start()
     {
+        int RoomNumber = 0;
+        int whichStartPoint = Random.Range(0,startPoints.Count);
+        roomList.Add( Instantiate(startRoom, startPoints[whichStartPoint].transform.position, Quaternion.identity));
+        roomList[RoomNumber].transform.SetParent(startPoints[whichStartPoint].transform);
 
+
+        for(int i = 0; startPoints.Count > i; i++)
+        {
+            if (whichStartPoint !=i)
+            {
+                roomList.Add(Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Count)], startPoints[i].transform.position, Quaternion.identity));
+                RoomNumber++;
+                roomList[RoomNumber].transform.SetParent(startPoints[i].transform);
+            }
+
+        }
+
+
+
+        for (int i = 0; points.Count > i; i++)
+        {
+           
+                roomList.Add(Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Count)], points[i].transform.position, Quaternion.identity));
+                RoomNumber++;
+                roomList[RoomNumber].transform.SetParent(points[i].transform);
+            
+
+        }
+
+        int whichBossPoint =Random.Range(0,bossPoints.Count);
+        
+        roomList.Add(Instantiate(bossRoom, startPoints[whichBossPoint].transform.position, Quaternion.identity));
+        RoomNumber++;
+        roomList[RoomNumber].transform.SetParent(startPoints[whichStartPoint].transform);
+        
+
+        for (int i = 0; bossPoints.Count > i; i++)
+        {
+            if (whichBossPoint !=i)
+            {
+                roomList.Add(Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Count)], bossPoints[i].transform.position, Quaternion.identity));
+                RoomNumber++;
+                roomList[RoomNumber].transform.SetParent(bossPoints[i].transform);
+            }
+
+        }
     }
 }
   
