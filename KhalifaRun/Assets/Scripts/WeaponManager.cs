224c12afebd1bcaec3f0025fdd1de7db3ee71ed6
Zsef <114775729+Balslev05@users.noreply.github.com>
2024-01-12 13:03:22 +0100

    Swapping Weapons :dog:

--- KhalifaRun/Assets/Scripts/WeaponManager.cs
+++ KhalifaRun/Assets/Scripts/WeaponManager.cs
@@ -1,49 +1,84 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEngine.Serialization;
 
 public class WeaponManager : MonoBehaviour
 {
-    public GameObject[] Weapons;
-    public GameObject weponHolder;
-    public Weapon_template weapon;
+    [Header("Assignebels")]
     public float pickupDistance;
     public Transform arms;
     public Transform handsHolder;
     public Camera cam;
-    public GameObject mouse;
     
-    
-    private Vector3 mousepos;
-    private float _distance = 10;
-
+    [Header("Bools")]
     public bool _gotWeapon;
     public bool attacking;
+    public bool Swapping_weapons;
+    
+    
+    [Header("Weapons")]
+    public GameObject[] close_Weapons;
+    public GameObject weponHolder;
+    public Weapon_template currentWeapon;
+    
     [Header("WeaponStats")]
         public int attackSpeed;
         public int windup;
         public float weight;
+    
+    [Header("Inventory")]
+    public List<Weapon_template> inventory = new List<Weapon_template>();
+    public int MaxweaponSlots;
+    public GameObject ItemDrop;
+    
+    
+    private Vector3 mousepos;
+    private float _distance = 10;
     // Start is called before the first frame update
 
     private void Update()
     {
-        Weapons = GameObject.FindGameObjectsWithTag("Item_Weapon");
+        
+        close_Weapons = GameObject.FindGameObjectsWithTag("Item_Weapon");
 
         arms.gameObject.SetActive(_gotWeapon);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && _gotWeapon)
+        if (Input.GetKeyDown(KeyCode.Alpha1) && inventory.Count > 0 && !attacking && !Swapping_weapons && currentWeapon != inventory[0])
+        {
+            StartCoroutine(SwapWeapons(inventory[0]));
+            updateWeapon(currentWeapon);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && inventory.Count > 1 && !attacking && !Swapping_weapons && currentWeapon != inventory[1])
+        {
+            StartCoroutine(SwapWeapons(inventory[1]));
+            updateWeapon(currentWeapon);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            DropWeapon();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _gotWeapon && !attacking && !Swapping_weapons)
         {
             PlayerChargeAttack();
         }
-        if (Input.GetKeyDown(KeyCode.Mouse1) && _gotWeapon)
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _gotWeapon && !attacking && !Swapping_weapons)
         {
             StartCoroutine(WeaponSpecialMove());
         }
         CheckforWeapons();
-    }
+        if (inventory.Count == 0)
+        {
+            currentWeapon = null;
+            _gotWeapon = false;
+        }
 
+    }
     public void CheckforWeapons()
     {
         if (ClosestWeapon() == null)
@@ -55,60 +90,124 @@ public class WeaponManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                GetWeapon();
+                GetWeapon(ClosestWeapon().GetComponent<Item_Weapon>().Weapon);
                 Destroy(ClosestWeapon());
             }
         }
     }
     GameObject ClosestWeapon()
     {
-        for (int i = 0; i < Weapons.Length; i++)
+        for (int i = 0; i < close_Weapons.Length; i++)
         {
-            _distance = Vector2.Distance(transform.position, Weapons[i].transform.position);
+            _distance = Vector2.Distance(transform.position, close_Weapons[i].transform.position);
             
             if (_distance <= pickupDistance)
             {
-                return Weapons[i];
+                return close_Weapons[i];
             }
         }   
         return null;
     }
-
-    // Update is called once per frame
-        
-        public void GetWeapon()
+    public void GetWeapon(Weapon_template weaponn)
         {
-            weapon = ClosestWeapon().GetComponent<Item_Weapon>().Weapon;
-
-            weponHolder.transform.localScale = weapon.size;
-            
-            if (weapon.TypeWeapon == Weapon_template.Type.Gun)
+            if (inventory.Count > MaxweaponSlots)
             {
-                weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Gun_sprite;
+                return;
+            }
+            
+            if (inventory.Count != MaxweaponSlots)
+            {
+                currentWeapon = weaponn;
+                inventory.Add(currentWeapon);
+            }
+
+            weponHolder.transform.localScale = currentWeapon.size;
+            
+            if (currentWeapon.TypeWeapon == Weapon_template.Type.Gun)
+            {
+                weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Gun_sprite;
                 
                 Debug.Log("PickUpGun");
             }
             
-            if (weapon.TypeWeapon == Weapon_template.Type.Melee)
+            if (currentWeapon.TypeWeapon == Weapon_template.Type.Melee)
             {
-                weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Melee_Sprite;
-                weight = weapon.Weight;
-                windup = weapon.windup;
+                weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Melee_Sprite;
+                weight = currentWeapon.Weight;
+                windup = currentWeapon.windup;
                 
                 Debug.Log("PickUpMelee");
             }
+            
             _gotWeapon = true;
         }
-
-        private void PlayerChargeAttack()
+    public void updateWeapon(Weapon_template weapon)
+    {
+        weponHolder.transform.localScale = currentWeapon.size;
+            
+        if (weapon.TypeWeapon == Weapon_template.Type.Gun)
+        {
+            weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Gun_sprite;
+                
+            Debug.Log("PickUpGun");
+        }
+            
+        if (weapon.TypeWeapon == Weapon_template.Type.Melee)
+        {
+            weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Melee_Sprite;
+            weight = currentWeapon.Weight;
+            windup = currentWeapon.windup;
+                
+            Debug.Log("PickUpMelee");
+        }
+    }
+    public IEnumerator SwapWeapons(Weapon_template swap2weapon)
+    {
+        
+        Swapping_weapons = true;
+        var localEulerAngles = handsHolder.localEulerAngles;
+        Vector3 weaponPose = new Vector3(localEulerAngles.x,localEulerAngles.y,localEulerAngles.z);
+        
+        handsHolder.transform.DOLocalRotate(new Vector3(weaponPose.x,weaponPose.y ,-70),0.4f).SetEase(Ease.OutBack);
+        
+        yield return new WaitForSeconds(0.4f);
+        
+        handsHolder.transform.DOLocalRotate(new Vector3(weaponPose.x,weaponPose.y ,weaponPose.z),0.4f).SetEase(Ease.OutBack);
+        
+        currentWeapon = swap2weapon;
+        weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Melee_Sprite;
+        weponHolder.transform.localScale = currentWeapon.size;
+        
+        yield return new WaitForSeconds(0.4f);
+        
+        Swapping_weapons = false;
+    }
+    public void DropWeapon()
+        {
+           if (inventory.Count == 0)
+           {
+                currentWeapon = null;
+               _gotWeapon = false;
+               return;
+           }
+           GameObject Instatiatetdrop = Instantiate(ItemDrop, transform.position,quaternion.identity);
+           
+           Instatiatetdrop.GetComponent<Item_Weapon>().Weapon = currentWeapon;
+           
+           inventory.Remove(currentWeapon);
+           
+           currentWeapon = inventory[0];
+           
+           updateWeapon(currentWeapon);
+            
+        }
+    private void PlayerChargeAttack()
         {
             attacking = true;
             
             var localEulerAngles = handsHolder.localEulerAngles;
             Vector3 weaponPose = new Vector3(localEulerAngles.x,localEulerAngles.y,localEulerAngles.z);
             Vector3 originalpose_handsholder = new Vector3(localEulerAngles.x,localEulerAngles.y,localEulerAngles.z);
-
-           
             
             handsHolder.transform.DOLocalRotate(new Vector3(weaponPose.x,weaponPose.y ,weaponPose.z =+ weight * 10),0.4f).SetEase(Ease.OutCirc);
 
@@ -116,8 +215,7 @@ public class WeaponManager : MonoBehaviour
             
             print("ChargedUp");
         }
-        
-         IEnumerator PlayerAttack(Vector3 pose_Handsholder)
+    IEnumerator PlayerAttack(Vector3 pose_Handsholder)
         {
             yield return new WaitForSeconds(windup);
             mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -129,66 +227,67 @@ public class WeaponManager : MonoBehaviour
             
             handsHolder.transform.DOLocalRotate(new Vector3(pose_Handsholder.x,pose_Handsholder.y ,pose_Handsholder.z),0.4f).SetEase(Ease.OutCubic);
             
-            weponHolder.transform.DOScale(weapon.size,0.4f).SetEase(Ease.OutCubic);
+            weponHolder.transform.DOScale(currentWeapon.size,0.4f).SetEase(Ease.OutCubic);
+            
+            yield return new WaitForSeconds(0.4f);
             
             attacking = false;
         }
-
-         public IEnumerator WeaponSpecialMove()
+    public IEnumerator WeaponSpecialMove()
          {
-             Debug.Log(weapon.Special);
-             if (weapon.Special.WeaponObject == SpecialMove_Template.Weapon.linial)
+             Debug.Log(currentWeapon.Special);
+             if (currentWeapon.Special.WeaponObject == SpecialMove_Template.Weapon.linial)
              {
-                 weponHolder.transform.DOScale(weapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
+                 weponHolder.transform.DOScale(currentWeapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
              }
-             
-             if (weapon.Special.WeaponObject == SpecialMove_Template.Weapon.lommelygte)
+             if (currentWeapon.Special.WeaponObject == SpecialMove_Template.Weapon.lommelygte)
              {
-                 weponHolder.transform.DORotate(new Vector3(0, 0, -60),0.4f).SetEase(Ease.OutCirc);
+                 weponHolder.transform.DORotate(new Vector3(0, 0, -90),0.4f).SetEase(Ease.OutCirc);
                  
                  yield return new WaitForSeconds(0.6f);
+                 weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Special.newSprite;
+
+                 Instantiate(currentWeapon.Special.instasiate.gameObject,weponHolder.transform.position,weponHolder.transform.localRotation,weponHolder.transform);
+                 weponHolder.transform.DetachChildren();
                  
-                 GameObject instamtiatet_Light = Instantiate(weapon.Special.instasiate.gameObject,transform.position,weponHolder.transform.localRotation);
                  
                  yield return new WaitForSeconds(0.4f);
-                 ResetRotation();
+                 
+                 weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Special.currentSprite;
+                 
+                 weponHolder.transform.DORotate(new Vector3(0, 0, 0),0.4f).SetEase(Ease.OutCirc);
+                 
              }
-             if (weapon.Special.WeaponObject == SpecialMove_Template.Weapon.umbrella)
+             if (currentWeapon.Special.WeaponObject == SpecialMove_Template.Weapon.umbrella)
              {
-                 weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Special.newSprite;
-                 weponHolder.transform.DOScale(weapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
+                 weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Special.newSprite;
+                 weponHolder.transform.DOScale(currentWeapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
                  weponHolder.transform.DORotate(new Vector3(0, 0, -60),0.4f).SetEase(Ease.OutCirc);
                  
                  yield return new WaitForSeconds(2);
-                 print("wadawda");
-                 weponHolder.GetComponent<SpriteRenderer>().sprite = weapon.Special.currentSprite;
-                 weponHolder.transform.DOScale(weapon.size,0.4f).SetEase(Ease.OutBounce);
-                 ResetRotation();
+                 
+                 weponHolder.GetComponent<SpriteRenderer>().sprite = currentWeapon.Special.currentSprite;
+                 weponHolder.transform.DOScale(currentWeapon.size,0.4f).SetEase(Ease.OutBounce);
+                 weponHolder.transform.DORotate(new Vector3(0, 0, 0),0.4f).SetEase(Ease.OutCirc);
                  //add knockback  
 
              }
-
-             if (weapon.Special.WeaponObject == SpecialMove_Template.Weapon.Computer)
+             if (currentWeapon.Special.WeaponObject == SpecialMove_Template.Weapon.Computer)
              {
-                 weponHolder.transform.DOScale(weapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
+                 weponHolder.transform.DOScale(currentWeapon.Special.newWeaponSize,0.4f).SetEase(Ease.OutBounce);
                  
                  yield return new WaitForSeconds(1);
                  
                  weponHolder.transform.DOScale(new Vector3(0,0,0),0.4f).SetEase(Ease.OutCirc);
                  PlayerAttack(weponHolder.transform.position);
+                 
                  yield return new WaitForSeconds(0.5f);
-                 weapon = null;
+                 
+                 currentWeapon = null;
                  _gotWeapon = false;
              }
          }
-
-
-         private void ResetRotation()
-         {
-             weponHolder.transform.DORotate(new Vector3(0, 0, 0),0.4f).SetEase(Ease.OutCirc);
-             
-             Debug.Log("reset");
-         }
+    
         
         
         
