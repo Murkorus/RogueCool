7e156270b141dceb7d139a93ec97923630f47c02
marcede9 <71291845+marcede9@users.noreply.github.com>
2024-01-15 14:07:23 +0100

    heaæ

---
+++ KhalifaRun/Assets/Scripts/Health Script.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    [SerializeField] int health = 6;
+    [SerializeField] GameObject[] heartContainers;
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            TakingDamage(1);
+        }
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            GainHearts(1);
+        }
+    }
+
+    public void TakingDamage(int Damage)
+    {
+
+        for (int i = 0; i < Damage; i++)
+        {
+            health--;
+            heartContainers[health].GetComponentInChildren<Image>().enabled = false;
+        }
+
+    }
+
+    public void GainHearts(int heal)
+    {
+        for(int i = 0; i< heal; i++)
+        {
+            heartContainers[health].GetComponentInChildren<Image>().enabled=true;
+            health++;
+        }
+    }
+
+}
